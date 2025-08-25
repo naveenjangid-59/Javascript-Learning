4ca6a631ff8159db1ed380f6e879ebe081da09b0
Naveen kumar <148692147+naveenjangid-59@users.noreply.github.com>
2025-08-25 17:10:19 +0000

    string methods continue

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+let name = "naveen"
+let repoCount = 18
+
+// concatenation
+// console.log("Hello " + name + ", your repo count is " + repoCount);  // outdated way
+console.log(`Hello ${name}, your repo count is ${repoCount}`); // modern way - template literals
+
+
+
+
+
+
+let myName = "naveen"
+console.log(myName[0]);
+
+console.log(myName.length);
+
