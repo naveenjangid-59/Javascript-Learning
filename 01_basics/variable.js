6e1fa51bc5768cd6761ced5545b44384305fe92a
Naveen kumar <148692147+naveenjangid-59@users.noreply.github.com>
2025-08-25 08:36:54 +0000

    learnt variables

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,1 +1,24 @@
-console.log("hello myself naveen")
+// total 5 ways
+
+const accountId = 893056;
+let accountEmail = "naveen@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 3. // not allowed - you can't change a const
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+accountEmail = "nvn@gmail.com"
+accountPassword = "8030"
+accountCity = "madras"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
