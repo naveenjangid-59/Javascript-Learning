2b898bb74f485bb362760d53695cd8f96263c028
Naveen kumar <148692147+naveenjangid-59@users.noreply.github.com>
2025-08-26 14:31:28 +0000

    learnt datetime

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,50 @@
+// Temporal.Now.instant() -- proposal, not in practice yet
+
+console.log(new Date());
+console.log(new Date().toISOString());
+console.log(new Date().toJSON());
+console.log(new Date().toLocaleDateString());
+console.log(new Date().toLocaleTimeString());
+console.log(new Date().toString());
+console.log(new Date().toTimeString());
+console.log(new Date().toLocaleString('default',{
+    weekday : "long",
+    month : "long"
+}));
+
+
+
+
+
+
+// **********************class notes
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
+
