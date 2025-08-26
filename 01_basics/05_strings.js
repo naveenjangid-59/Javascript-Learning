4b40fc2121ad751d30a5235ea65326d7cc9015f0
Naveen kumar <148692147+naveenjangid-59@users.noreply.github.com>
2025-08-26 13:15:44 +0000

    learnt about strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,12 +6,25 @@ let repoCount = 18
 console.log(`Hello ${name}, your repo count is ${repoCount}`); // modern way - template literals
 
 
-
-
+// another way to declare strings
+const gameName = new String('Smash-Karts')
 
 
 let myName = "naveen"
 console.log(myName[0]);
-
 console.log(myName.length);
+console.log(myName.__proto__);
+
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('K'));
+console.log(gameName.substring(4,1)); //swaps start,end if start > end
+console.log(gameName.slice(-14));
+console.log(gameName.trim());
+console.log(gameName.replace('-','-the-'));
+console.log(gameName.split('-'));
+
+
+
+
 
